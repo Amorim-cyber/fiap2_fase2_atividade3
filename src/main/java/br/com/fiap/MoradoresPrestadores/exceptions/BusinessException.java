6057e4723643955715b24cf034ae5f9656af02e0
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-28 20:56:06 -0300

    Adiciona BusinessException.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/exceptions/BusinessException.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/exceptions/BusinessException.java
@@ -1,2 +1,7 @@
-package br.com.fiap.MoradoresPrestadores.exceptions;public class BusinessException {
+package br.com.fiap.MoradoresPrestadores.exceptions;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message){
+        super(message);
+    }
 }
