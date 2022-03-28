d1954db498b917333d898365283858764569b8b5
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-28 15:03:05 -0300

    Adiciona Usuario.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/model/Usuario.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/model/Usuario.java
@@ -1,2 +1,21 @@
-package br.com.fiap.MoradoresPrestadores.model;public class Usuario {
+package br.com.fiap.MoradoresPrestadores.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name="tb_usuario")
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Usuario {
+
+    @Id
+    @SequenceGenerator(name="usuario",sequenceName="sq_tb_usuario",allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="usuario")
+    @Column(name="id_usuario")
+    private int id;
+
+    @Column(name="login",nullable=false,length=20)
+    protected String login;
+
+    @Column(name="senha",nullable=false,length=20)
+    protected String senha;
+
 }
