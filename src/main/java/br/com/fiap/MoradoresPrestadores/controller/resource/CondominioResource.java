27cd383c58e6b8a4a87374638ac20493dcc13e58
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-28 20:53:32 -0300

    Adiciona CondominioResource.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/controller/resource/CondominioResource.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/controller/resource/CondominioResource.java
@@ -1,2 +1,62 @@
-package br.com.fiap.MoradoresPrestadores.controller.resource;public class CondominioResource {
+package br.com.fiap.MoradoresPrestadores.controller.resource;
+
+import br.com.fiap.MoradoresPrestadores.exceptions.BusinessException;
+import br.com.fiap.MoradoresPrestadores.exceptions.NotFoundException;
+import br.com.fiap.MoradoresPrestadores.model.Condominio;
+import br.com.fiap.MoradoresPrestadores.repository.CondominioRepository;
+import br.com.fiap.MoradoresPrestadores.util.MessageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("condominio")
+public class CondominioResource {
+
+    @Autowired
+    private CondominioRepository condominioRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Condominio> findAll(){
+        return condominioRepository.findAll();
+    }
+
+    @GetMapping(value="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Condominio findById(@PathVariable int id){
+        return condominioRepository.findById(id).orElseThrow(NotFoundException::new);
+    }
+
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Condominio save(@RequestBody Condominio condominio){
+
+        Optional<Condominio> condominioOptional =
+                condominioRepository.findByIdAndNome(condominio.getId(),condominio.getNome());
+
+        if(condominioOptional.isPresent())
+            throw new BusinessException(MessageUtils.CONDOMINIO_ALREADY_EXISTS);
+
+        return condominioRepository.save(condominio);
+    }
+
+    @PutMapping(value="/{id}",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Condominio update(@RequestBody Condominio condominio,@PathVariable int id){
+
+        condominio.setId(id);
+        return condominioRepository.save(condominio);
+    }
+
+    @DeleteMapping(value="/{codigo}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Condominio delete(int id){
+        Condominio condominio = findById(id);
+
+        condominioRepository.deleteById(condominio.getId());
+
+        return condominio;
+    }
+
 }
