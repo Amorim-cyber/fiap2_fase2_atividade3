3685eec4d6dc47f99d8603d1d2f279c9f79a6fac
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-28 20:58:44 -0300

    Adiciona CondominioRepository.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/repository/CondominioRepository.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/repository/CondominioRepository.java
@@ -1,2 +1,14 @@
-package br.com.fiap.MoradoresPrestadores.repository;public interface CondominioRepository {
+package br.com.fiap.MoradoresPrestadores.repository;
+
+import br.com.fiap.MoradoresPrestadores.model.Condominio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CondominioRepository extends JpaRepository<Condominio,Integer> {
+
+    Optional<Condominio> findByIdAndNome(int id, String nome);
+
 }
