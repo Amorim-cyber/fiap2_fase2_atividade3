3b9d3b0965aba6c27155b7b25235f4235c96c5d7
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-28 13:51:58 -0300

    Adiciona Morador.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/model/Morador.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/model/Morador.java
@@ -1,2 +1,71 @@
-package br.com.fiap.MoradoresPrestadores.model;public class Morador {
+package br.com.fiap.MoradoresPrestadores.model;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="tb_morador")
+public class Morador {
+
+    @Id
+    @SequenceGenerator(name="morador",sequenceName="sq_tb_morador",allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="morador")
+    @Column(name="id_morador")
+    private int id;
+
+    @Column(name="nm_morador",nullable=false,length=100)
+    private String nome;
+
+
+
+    @ManyToMany(cascade=CascadeType.PERSIST)
+    @JoinTable(joinColumns = @JoinColumn(name="id_morador"),
+            inverseJoinColumns = @JoinColumn(name="id_morada"), name = "tb_registro_morada")
+    private List<Morada> moradas;
+
+    @OneToMany(mappedBy = "morador")
+    private List<Registro> registros;
+
+    public Morador() {
+    }
+
+    public Morador(int id, String nome, List<Morada> moradas, List<Registro> registros) {
+        this.id = id;
+        this.nome = nome;
+        this.moradas = moradas;
+        this.registros = registros;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Morada> getMoradas() {
+        return moradas;
+    }
+
+    public void setMoradas(List<Morada> moradas) {
+        this.moradas = moradas;
+    }
+
+    public List<Registro> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(List<Registro> registros) {
+        this.registros = registros;
+    }
+
+
 }
