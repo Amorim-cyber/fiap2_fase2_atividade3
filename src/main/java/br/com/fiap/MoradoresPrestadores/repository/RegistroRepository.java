244536e7ec4aaf7ba66d57291d9fdb99e8b43e71
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-29 20:58:49 -0300

    Adiciona RegistroRepository.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/repository/RegistroRepository.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/repository/RegistroRepository.java
@@ -1,2 +1,16 @@
-package br.com.fiap.MoradoresPrestadores.repository;public interface RegistroRepository {
+package br.com.fiap.MoradoresPrestadores.repository;
+
+import br.com.fiap.MoradoresPrestadores.model.Morador;
+import br.com.fiap.MoradoresPrestadores.model.Prestador;
+import br.com.fiap.MoradoresPrestadores.model.Registro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RegistroRepository extends JpaRepository<Registro,Integer> {
+
+    Optional<Registro> findMoradorAndPrestador(Morador morador, Prestador prestador);
+
 }
