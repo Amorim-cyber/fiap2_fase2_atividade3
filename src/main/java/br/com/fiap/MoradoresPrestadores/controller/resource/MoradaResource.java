b9f63409b61f32925d6b92e35dba0b23773d46a5
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-29 16:13:54 -0300

    Adiciona MoradaResource.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/controller/resource/MoradaResource.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/controller/resource/MoradaResource.java
@@ -1,2 +1,61 @@
-package br.com.fiap.MoradoresPrestadores.controller.resource;public class MoradaResource {
+package br.com.fiap.MoradoresPrestadores.controller.resource;
+
+import br.com.fiap.MoradoresPrestadores.exceptions.BusinessException;
+import br.com.fiap.MoradoresPrestadores.exceptions.NotFoundException;
+import br.com.fiap.MoradoresPrestadores.model.Morada;
+import br.com.fiap.MoradoresPrestadores.repository.MoradaRepository;
+import br.com.fiap.MoradoresPrestadores.util.MessageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("morada")
+public class MoradaResource {
+
+    @Autowired
+    private MoradaRepository moradaRepository;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Morada> findAll(){return moradaRepository.findAll();}
+
+    @GetMapping(value="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Morada findById(@PathVariable int id){
+        return moradaRepository.findById(id).orElseThrow(NotFoundException::new);
+    }
+
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Morada save(@RequestBody Morada morada) {
+
+        Optional<Morada> moradaOptional =
+                moradaRepository.findByNumeroAndCondominio(morada.getNumero(), morada.getCondominio());
+
+        if(moradaOptional.isPresent())
+            throw new BusinessException(MessageUtils.MORADA_ALREADY_EXISTS);
+
+        return moradaRepository.save(morada);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Morada update(@RequestBody Morada morada){
+
+        findById(morada.getId());
+
+        return moradaRepository.save(morada);
+
+    }
+
+    @DeleteMapping(value="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Morada delete(int id){
+        Morada morada = findById(id);
+
+        moradaRepository.deleteById(morada.getId());
+
+        return morada;
+    }
 }
