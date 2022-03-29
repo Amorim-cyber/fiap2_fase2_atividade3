8a117f83c859d16507e89679360f0f26b3aef71f
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-29 17:40:08 -0300

    Adiciona MoradorRepository.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/repository/MoradorRepository.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/repository/MoradorRepository.java
@@ -1,2 +1,14 @@
-package br.com.fiap.MoradoresPrestadores.repository;public interface MoradorRepository {
+package br.com.fiap.MoradoresPrestadores.repository;
+
+import br.com.fiap.MoradoresPrestadores.model.Morador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MoradorRepository extends JpaRepository<Morador,Integer> {
+
+    Optional<Morador> findByLoginAndSenha(String login, String senha);
+
 }
