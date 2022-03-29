26d841c03142a8b685cae8e511205336821b8926
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-29 15:34:25 -0300

    Atualiza MessageUtils.java [1]

--- src/main/java/br/com/fiap/MoradoresPrestadores/util/MessageUtils.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/util/MessageUtils.java
@@ -5,4 +5,7 @@ public abstract class MessageUtils {
     public static final String NO_RECORDS_FOUND = "No records found";
 
     public static final String CONDOMINIO_ALREADY_EXISTS = "Condominio already exists in the database";
+
+    public static final String INVALID_COMMAND = "Invalid Command";
+
 }
