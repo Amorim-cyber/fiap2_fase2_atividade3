0eb0565242486ebef3b4fea775905f876fb99ba3
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-30 11:23:13 -0300

    Atualiza RegistroRepository.java [1]

--- src/main/java/br/com/fiap/MoradoresPrestadores/repository/RegistroRepository.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/repository/RegistroRepository.java
@@ -4,6 +4,7 @@ import br.com.fiap.MoradoresPrestadores.model.Morador;
 import br.com.fiap.MoradoresPrestadores.model.Prestador;
 import br.com.fiap.MoradoresPrestadores.model.Registro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +12,9 @@ import java.util.Optional;
 @Repository
 public interface RegistroRepository extends JpaRepository<Registro,Integer> {
 
+    @Query("SELECT registro "+
+            "FROM Registro registro "+
+            "WHERE registro.morador = :morador AND registro.prestador = :prestador")
     Optional<Registro> findMoradorAndPrestador(Morador morador, Prestador prestador);
 
 }
