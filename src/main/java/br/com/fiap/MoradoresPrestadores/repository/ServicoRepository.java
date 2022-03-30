987168c689fdd290c4458be06433e9b05cfaedda
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-29 21:16:34 -0300

    Adiciona ServicoRepository.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/repository/ServicoRepository.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/repository/ServicoRepository.java
@@ -1,2 +1,15 @@
-package br.com.fiap.MoradoresPrestadores.repository;public interface ServicoRepository {
+package br.com.fiap.MoradoresPrestadores.repository;
+
+import br.com.fiap.MoradoresPrestadores.model.Ocupacao;
+import br.com.fiap.MoradoresPrestadores.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ServicoRepository extends JpaRepository<Servico,Integer> {
+
+    Optional<Servico> findByOcupacao(Ocupacao ocupacao);
+
 }
