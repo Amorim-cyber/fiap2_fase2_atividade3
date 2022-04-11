d66b1172b63e61c6f1896a118d5621126aa3b87e
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-04-11 20:06:50 -0300

    Atualiza FormController.java

--- src/main/java/br/com/fiap/MoradoresPrestadores/controller/FormController.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/controller/FormController.java
@@ -1,2 +1,17 @@
-package br.com.fiap.MoradoresPrestadores.controller;public class FormController {
+package br.com.fiap.MoradoresPrestadores.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("formulario")
+public class FormController {
+
+    @GetMapping()
+    public String open(Model model){
+        return "formulario/form";
+    }
+
 }
