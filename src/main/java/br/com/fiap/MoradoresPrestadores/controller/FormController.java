83a8848fe8c742cf38b620494b2e274af8430180
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-04-18 10:49:43 -0300

    Atualiza FormController.java [2]

--- src/main/java/br/com/fiap/MoradoresPrestadores/controller/FormController.java
+++ src/main/java/br/com/fiap/MoradoresPrestadores/controller/FormController.java
@@ -1,17 +1,45 @@
 package br.com.fiap.MoradoresPrestadores.controller;
 
+import br.com.fiap.MoradoresPrestadores.controller.resource.PrestadorResource;
+import br.com.fiap.MoradoresPrestadores.controller.resource.ServicoResource;
+import br.com.fiap.MoradoresPrestadores.model.*;
+import br.com.fiap.MoradoresPrestadores.repository.PrestadorRepository;
+import br.com.fiap.MoradoresPrestadores.repository.ServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("formulario")
 public class FormController {
 
+    @Autowired
+    private ServicoResource servicoResource;
+
+
     @GetMapping()
     public String open(Model model){
         return "formulario/form";
     }
+
+    @GetMapping("usuario")
+    public String reOpen(Model model,
+                         Prestador prestador,
+                         Morador morador,
+                         Morada morada,
+                         Condominio condominio,
+                         HttpServletRequest request){
+        model.addAttribute("servicos",servicoResource.findAll());
+        model.addAttribute("usuario",request.getParameter("tipo"));
+        System.out.println(model.getAttribute("usuario").equals("prestador"));
+        return "formulario/form";
+    }
+
 
 }
